507850bafc65dc2c73a441b4a739cc816f9dabdf
Lazar Tatomir <lazar.tatomir.25@singimail.rs>
2025-10-03 16:50:08 +0200

    Add Math excersise file of day 2

---
+++ day2math.m
@@ -0,0 +1,19 @@
+# Excersise I:
+a = 2; b = 1;
+result = (a/b)^3 * (a/b)^-5
+
+# Excersise II:
+((1+i*sqrt(7))^4 + (1-i*sqrt(7))^4) / (-1+i*sqrt(3))^4 + (-1-i*sqrt(3))^4
+
+# Excersise III:
+(cos(17*pi/6) * sin(7*pi/3) * tan(17*pi/4)) / (cot(10*pi/3) * cos(7*pi/4) * sin(8*pi/3))
+
+# Excersise IV:
+ime = "Lazar";
+prezime = "Tatomir";
+# I nacin:
+ime_prezime = cstrcat(name, lastname)
+# II nacin:
+name_lastname = [name," ",lastname]
+
+
